235de1979e07fd2af1a3e3557902967827971a9f
Victor Murta <victor.murta97@gmail.com>
2019-11-26 15:12:43 -0500

    modified how charactters are emplaced back onto pipe

--- HW5server.c
+++ HW5server.c
@@ -249,7 +249,12 @@ void execution_service() {
                         }
                     }                   
                 } 
-                putchar('\0');
+                //manually add null terminator
+                rc = Socket_putc('\0', connect_socket);
+                if (rc == EOF) {
+                    printf("Socket_putc EOF or error\n");             
+                    return;  /* assume socket EOF ends service for this client */
+                }
                 fclose(read_handle);
             }
 
